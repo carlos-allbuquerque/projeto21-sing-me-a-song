a447c8e8252cfa498df37d795e364936e067cb0d
carlos <caarlos.albuquerq@gmail.com>
2022-09-26 14:34:29 -0300

    test: add fail trying to create a recommendation unitary test

--- back-end/tests/unitary/recommendationsService.test.ts
+++ back-end/tests/unitary/recommendationsService.test.ts
@@ -9,6 +9,13 @@ describe("recommendationService test suite", () => {
     youtubeLink: "https://www.youtube.com/watch",
   };
 
+  const recommendationFactory = {
+    id: 1,
+    name: "random name",
+    youtubeLink: "https://www.youtube.com/watch",
+    score: 2,
+  };
+
   describe("Create recommendation tests suites", () => {
     it("Sucess in create recommendation", async () => {
       const { name } = recommendationNameAndLink;
@@ -24,6 +31,17 @@ describe("recommendationService test suite", () => {
       await recommendationService.insert(recommendationNameAndLink);
       expect(recommendationRepository.findByName).toHaveBeenCalledWith(name);
       expect(recommendationRepository.create).toHaveBeenCalledTimes(1);
+    });
+
+    it("Fail trying create recommendation", async () => {
+      jest
+        .spyOn(recommendationRepository, "findByName")
+        .mockResolvedValueOnce(recommendationFactory);
+
+      expect(recommendationService.insert(recommendationNameAndLink)).rejects.toEqual({
+        message: "Recommendations names must be unique",
+        type: "conflict",
+      });
     });
   });
 });
