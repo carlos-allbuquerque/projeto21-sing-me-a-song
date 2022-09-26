5a2e9ca5f3c62a225b306357b73061f87b26b1c0
carlos <caarlos.albuquerq@gmail.com>
2022-09-26 17:39:54 -0300

    test: add a song recommendation test

--- front-end/cypress/e2e/recommendations.cy.js
+++ front-end/cypress/e2e/recommendations.cy.js
@@ -4,4 +4,28 @@
 beforeEach(() => {
     cy.clearDatabase();
 });
-  
+
+describe("Recommendations test suite", () => {
+    describe("Create a song recommendation", () => {
+        it("should add a song recommendation", () => {
+            cy.visit("http://localhost:3000/");
+    
+            cy.get("input[placeholder='Name']").type(
+                "Diego Pinho - Caractere mais frequente"
+            );
+            cy.get("input[placeholder='https://youtu.be/...']").type(
+                "https://youtu.be/q08oqgoSTSo"
+            );
+    
+            cy.intercept("POST", "http://localhost:5000/recommendations").as(
+                "createRecommendation"
+            );
+            cy.get("button").click();
+    
+            cy.wait("@createRecommendation").then((res) => {
+                expect(res.response.statusCode).to.equals(201);
+            });
+        });
+    });
+});
+
